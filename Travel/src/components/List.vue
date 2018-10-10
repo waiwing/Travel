9433b5f7f27868908545d81457c2651210fcf58c
“waiwing <waiwing14@gmail.com>
2018-10-10 23:27:16 +0800

    project init

---
+++ Travel/src/components/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>this is a list</h1>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style scoped>
+
+</style>
--- Travel/src/main.js
+++ Travel/src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick' // npm install fastClick -- save 解决点击300毫秒的问题
+import './assets/styles/reset.css' // 统一css
+import './assets/styles/border.css' // 1像素问题
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
